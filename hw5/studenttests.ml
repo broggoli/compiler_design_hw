ddcc2f9c3b54ed838cb6969d7c3339e11ce2f4a9
bibin <bibin@soi.ch>
2020-12-02 19:06:33 +0100

    added unit tests

--- hw5/studenttests.ml
+++ hw5/studenttests.ml
@@ -13,7 +13,26 @@ let hard_tests = [
   ("oatprograms/linked_list.oat", "", "6");
 ]
 
+let unit_tests = [
+  "typecheck_ty_fun_ok",
+    (fun () ->
+       try
+        Typechecker.typecheck_ty (Ast.no_loc []) Tctxt.empty (TNullRef (RFun ([TInt; TRef RString; TNullRef (RArray TBool)], RetVoid)));
+       with
+       | Typechecker.TypeError s -> failwith "should not fail"
+    )
+; ("typecheck_ty_fun_fail",
+    (fun () ->
+       try
+        Typechecker.typecheck_ty (Ast.no_loc []) Tctxt.empty (TNullRef (RFun ([TInt; TRef RString; TNullRef (RArray TBool); TNullRef (RStruct "A")], RetVoid)));
+        failwith "should not succeed"
+       with
+       | Typechecker.TypeError s -> ()
+    )
+  )
+]
+
 let provided_tests : suite = [
   Test("hard_tests provided", executed_oat_file hard_tests);
-  Test("easy_array_tests provided", executed_oat_file easy_array_tests);
+  Test("student unit_tests", unit_tests)
 ] 
